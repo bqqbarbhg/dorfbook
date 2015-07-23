c5b8d62a0570607885fe6623ac1e38f706ec134a
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-23 22:48:24 +0300

    Test stat route

--- test/test_routes.py
+++ test/test_routes.py
@@ -29,6 +29,9 @@ t = Tester()
 r = requests.get('http://127.0.0.1:3500/')
 t.check(r.status_code == 200, 'Can get root')
 
+r = requests.get('http://127.0.0.1:3500/stats')
+t.check(r.status_code == 200, 'Can get stats')
+
 r = requests.get('http://127.0.0.1:3500/sdoijfiosdjf')
 t.check(r.status_code == 404, 'Random route gives 404')
 
