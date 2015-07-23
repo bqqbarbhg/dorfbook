34e4900d06ed1f796731a2d1fcbffe0a477936ad
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-23 23:28:39 +0300

    Better route test

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,9 +1,11 @@
 
-r = requests.get('http://127.0.0.1:3500/')
-t.check(r.status_code == 200, 'Can get root')
+def dorf_get(route):
+	return requests.get('http://127.0.0.1:3500' + route)
 
-r = requests.get('http://127.0.0.1:3500/stats')
-t.check(r.status_code == 200, 'Can get stats')
+expect_routes = ['/', '/dwarves', '/locations', '/feed', '/stats']
+for route in expect_routes:
+	r = dorf_get(route)
+	t.check(r.status_code == 200, "Can get '%s'" % route)
 
-r = requests.get('http://127.0.0.1:3500/sdoijfiosdjf')
+r = dorf_get('/sdoijfiosdjf')
 t.check(r.status_code == 404, 'Random route gives 404')
