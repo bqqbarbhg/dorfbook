88adab20856f45d85d2e4f5b70fe57f73dd76450
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2015-07-23 11:21:46 +0300

    Added a few https status codes.

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,70 @@ struct HTTP_Status_Description {
 	int status_code;
 	const char *description;
 } http_status_descriptions[] = {
+    /* 1xx Informational */
+    { 100, "Continue" },
+    { 101, "Switching Protocols" },
+    { 102, "Processing" },
+
+    /* 2xx Success */
 	{ 200, "OK" },
-	{ 404, "Not found" },
+    { 201, "Created" },
+    { 202, "Accepted" },
+    { 203, "Non-Authoritative Information" },
+    { 204, "No Content" },
+    { 205, "Reset Content" },
+    { 206, "Partial Content" },
+    { 207, "Multi-Status" },
+    { 208, "Already Reported" },
+    { 226, "IM Used" },
+
+    /* 3xx Redirection */
+    { 300, "Multiple Choices" },
+    { 301, "Moved Permanently" },
+    { 302, "Found" },
+    { 303, "See Other" },
+    { 304, "Not Modified" },
+    { 305, "Use Proxy" },
+    /* { 306, "Switch Proxy" }, */
+    { 307, "Temporary Redirect" },
+    { 308, "Permanent Redirect" },
+
+    /* 4xx Client Error */
+	{ 400, "Bad Request" },
+    { 401, "Unauthorized" },
+    { 402, "Payment Required" },
+    { 403, "Forbidden" },
+    { 404, "Not Found" },
+    { 405, "Method Not Allowed" },
+    { 406, "Not Acceptable" },
+    { 407, "Proxy Authentication Required" },
+    { 408, "Request Timeout" },
+    { 409, "Conflict" },
+    { 410, "Gone" },
+    { 411, "Length Required" },
+    { 412, "Precondition Failed" },
+    { 413, "Payload Too Large" },
+    { 414, "Request-URI Too Long" },
+    { 415, "Unsupported Media Type" },
+    { 416, "Requested Range Not Satisfiable" },
+    { 417, "Expectation Failed" },
+    { 418, "I'm a teapot" },
+    { 420, "Blaze it" },
+
+    /* 5xx Server Error */
+    { 500, "Internal Server Error" },
+    { 501, "Not Implemented" },
+    { 502, "Bad Gateway" },
+    { 503, "Service Unavailable" },
+    { 504, "Gateway Timeout" },
+    { 505, "HTTP Version Not Supported" },
+    { 506, "Variant Also Negotiates" },
+    { 507, "Insufficient Storage" },
+    { 508, "Loop Detected" },
+    /* { 509, "Bandwidth Limit Exceeded" }, */
+    { 510, "Not Extended" },
+    { 511, "Network Authentication Required" },
+    /* { 520, "Unknown Error" }, */
 };
 
 const char *get_http_status_description(int status_code) {
