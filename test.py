f9764f7d762d069c23cdcb3d33fc705b4858d641
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-23 20:05:39 +0000

    Separate tests and test runner

---
+++ test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import requests
+from collections import namedtuple
+from glob import glob
+
+Fail = namedtuple("Fail", ("filename", "line", "message"))
+def format_fail(fail):
+	loc = '%s:%d' % (fail.filename, fail.line)
+	return '%30s  %s' % (loc, fail.message)
+
+class Tester:
+	def __init__(self):
+		self.fail_list = []
+		self.num_total = 0
+
+	def check(self, condition, message):
+		if not condition:
+			frame = sys._getframe(1)
+			filename = frame.f_code.co_filename
+			line = frame.f_lineno
+
+			fail = Fail(filename, line, message)
+			self.fail_list.append(fail)
+		self.num_total += 1
+
+t = Tester()
+
+for testfile in glob('test/*.py'):
+	execfile(testfile)
+
+print 'Tests passed: %d/%d' % (t.num_total - len(t.fail_list), t.num_total)
+if t.fail_list:
+	print 'Failed tests:'
+	print '\n'.join('%s' % format_fail(fail) for fail in t.fail_list)
+
+sys.exit(len(t.fail_list))
+
