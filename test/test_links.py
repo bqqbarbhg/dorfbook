26b1841f17edba59eff0e32bd1f8a055d3e19bb9
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-24 03:48:25 +0300

    Also crawl images

--- test/test_links.py
+++ test/test_links.py
@@ -8,10 +8,16 @@ while len(to_crawl) > len(crawled):
 	r = dorf_get(url)
 	extra = 'from %s' % got_from[url]
 	if t.check(r.status_code == 200, 'Can reach %s' % url, extra):
-		soup = BeautifulSoup(r.text, 'html.parser')
-		for a_tag in soup.find_all('a'):
-			href = a_tag.get('href')
-			if href not in to_crawl:
-				to_crawl.add(href)
-				got_from[href] = url
+		if r.headers['Content-Type'] == 'text/html':
+			soup = BeautifulSoup(r.text, 'html.parser')
+			for a_tag in soup.find_all('a'):
+				href = a_tag.get('href')
+				if href not in to_crawl:
+					to_crawl.add(href)
+					got_from[href] = url
+			for img_tag in soup.find_all('img'):
+				href = img_tag.get('src')
+				if href not in to_crawl:
+					to_crawl.add(href)
+					got_from[href] = url
 
