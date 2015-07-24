afcf8ab21d658062b9d95fbd9f938d2ef7aa8699
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-24 04:03:38 +0300

    Test that svg:s are valid xml

--- test/test_links.py
+++ test/test_links.py
@@ -1,4 +1,6 @@
 
+from xml.etree import ElementTree as XmlTree
+
 to_crawl = {'/', '/dwarves', '/locations', '/feed', '/stats'}
 crawled = set()
 got_from = { url: '(root)' for url in to_crawl }
@@ -8,7 +10,8 @@ while len(to_crawl) > len(crawled):
 	r = dorf_get(url)
 	extra = 'from %s' % got_from[url]
 	if t.check(r.status_code == 200, 'Can reach %s' % url, extra):
-		if r.headers['Content-Type'] == 'text/html':
+		content_type = r.headers['Content-Type']
+		if content_type == 'text/html':
 			soup = BeautifulSoup(r.text, 'html.parser')
 			for a_tag in soup.find_all('a'):
 				href = a_tag.get('href')
@@ -20,4 +23,13 @@ while len(to_crawl) > len(crawled):
 				if href not in to_crawl:
 					to_crawl.add(href)
 					got_from[href] = url
+		elif content_type == 'image/svg+xml':
+			valid = True
+			xml_extra = None
+			try:
+				XmlTree.fromstring(r.text)
+			except XmlTree.ParseError as e:
+				xml_extra = '%s, %s' % (extra, 'at %d:%d' % e.position)
+				valid = False
+			t.check(valid, '%s is valid xml' % url, xml_extra)
 
