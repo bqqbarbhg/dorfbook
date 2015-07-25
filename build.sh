2d1ffd190149d08dbcc202ee67a553b3a1e125e9
Samuli Raivio <samuli.1995@hotmail.com>
2015-07-25 19:23:44 +0300

    Remove pthread, try exe relative path

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir pre 2> /dev/null
 mkdir data 2> /dev/null
 
 cp -r data bin
-gcc src/pre/pre_build.cpp -g -lm -lrt -pthread -o bin/pre_dorfbook
-bin/pre_dorfbook gen/pre_output.cpp
+gcc src/pre/pre_build.cpp -g -lm -lrt -o bin/pre_dorfbook
+bin/pre_dorfbook ../gen/pre_output.cpp
 gcc src/build.cpp -g -lm -lrt -pthread -o bin/dorfbook
 
