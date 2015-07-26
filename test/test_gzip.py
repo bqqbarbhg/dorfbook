6068f34b015e72330bba466e51cd45c378f2041d
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-26 22:14:39 +0300

    Switch to fixtures

--- test/test_gzip.py
+++ test/test_gzip.py
@@ -3,34 +3,39 @@ import random
 import zlib
 import gzip
 
-for _ in range(5):
-	length = random.randint(1, 1000)
-	arr = str(random.choice(string.printable) for _ in range(length))
+fixtures = [
+	(bytearray([]), "Empty"),
+	(bytearray([0]), "Single zero"),
+	(bytearray(list(range(256))), "All sequential bytes"),
+	(bytearray([0xAB, 0xCD, 0xEF, 0x99] * 2000), "2000x ABCDEF99"),
+]
 
-	dorf_crc = int(test_call("crc32", arr))
-	zlib_crc = zlib.crc32(arr) & 0xFFFFFFFF
+for data, desc in fixtures:
 
-	t.check(dorf_crc == zlib_crc, "crc32 is compatible with zlib")
+	dorf_crc = int(test_call("crc32", data))
+	zlib_crc = zlib.crc32(str(data)) & 0xFFFFFFFF
+
+	t.check(dorf_crc == zlib_crc, "crc32 is compatible with zlib", desc)
 
 
-for _ in range(5):
-	length = random.randint(1, 10000)
-	content = str(random.choice(string.printable) for _ in range(length))
+for data, desc in fixtures:
 
-	dorf_gzip = test_call("gzip", content)
+	dorf_gzip = test_call("gzip", data)
 	success = False
 	extra = None
 	try:
 		with gzip.GzipFile(fileobj=StringIO(dorf_gzip)) as gzip_file:
 			content_round_trip = gzip_file.read()
-			if content_round_trip == content:
+			if content_round_trip == data:
 				success = True
+			else:
+				extra = 'mismatch'
 	except zlib.error as err:
 		extra = 'zlib: %s' % str(err.message)
 	except IOError as err:
 		extra = 'io: %s' % str(err.message)
 
-	t.check(success, "gzip round trips", extra)
+	t.check(success, "gzip round trips", '%s, %s' % (extra, desc))
 
 
 
